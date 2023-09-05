d3be9ec588d8fef376e792e27bca84a5196457a6
vitalii <vitaliisili@yahoo.com>
2023-09-06 00:46:20 +0200

    🍱 Add or update assets.

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,4 +1,5 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.auth import models as authmodel
-class User(authmodel.User):
+class User(AbstractUser):
     pass
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from apps.user.forms import SignupForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = SignupForm()
+    return render(request, 'user/signup.html', {'form': form})
