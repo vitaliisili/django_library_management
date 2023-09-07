735348a37e320dd5101ec781fd10339160f7950e
Vitalii Sili <vitaliisili@yahoo.com>
2023-09-08 00:57:54 +0200

    👔 Add or update business logic, Add a Category model

--- apps/library/models.py
+++ apps/library/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from apps.core.models import BaseModel
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=70)
+    description = models.TextField()
+    is_fiction = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
