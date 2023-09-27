54b047f54341cb6d5d8cc0207ef9692c04f5bbf3
Vitalii Sili <vitaliisili@yahoo.com>
2023-09-27 12:02:08 +0200

    🔧 Add or update configuration files.

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from apps.core.views import HandleNotFound
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("apps.library.urls")),
     path('auth/', include("apps.user.urls")),
 ]
+
+handler404 = HandleNotFound.as_view()
