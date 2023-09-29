a81f7cef31b29d3cd5b7c0e2a1aa685414c29b69
Vitalii Sili <vitaliisili@yahoo.com>
2023-09-29 16:50:26 +0200

    ♻️ ✨ Introduce new features.

--- config/settings.py
+++ config/settings.py
@@ -95,8 +95,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-# MEDIA_URL = "/media/"
-# MEDIA_ROOT = Path(BASE_DIR, 'mediafiles')
+MEDIA_URL = "media/"
+MEDIA_ROOT = Path(BASE_DIR, 'media/')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
