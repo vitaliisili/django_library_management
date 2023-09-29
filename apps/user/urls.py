ced025c29ed442284de673a5e668390a5d2c97b4
Vitalii Sili <vitaliisili@yahoo.com>
2023-09-29 13:22:06 +0200

    ♻️ Refactor code. Remove signup url based on function

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from apps.user import views
 
 urlpatterns = [
-    # path('sign-up', views.signup, name='sign-up'),
     path('sign-up', views.SignUpView.as_view(), name='sign-up'),
     path('sign-in', views.SignInView.as_view(), name='sign-in'),
     path('logout', views.UserLogoutView.as_view(), name='logout'),
